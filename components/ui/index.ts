5d0c9024620b4c22770aa678f3aa0e1455c292fa
Yespecom <yespecomstudio@gmail.com>
2025-08-21 21:11:19 +0530

    Create index.ts

---
+++ components/ui/index.ts
@@ -0,0 +1,66 @@
+export { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "./accordion"
+export { Alert, AlertDescription, AlertTitle } from "./alert"
+export {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "./alert-dialog"
+export { Avatar, AvatarFallback, AvatarImage } from "./avatar"
+export { Badge, badgeVariants } from "./badge"
+export { Button, buttonVariants } from "./button"
+export { Calendar } from "./calendar"
+export { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "./card"
+export { Checkbox } from "./checkbox"
+export {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "./dialog"
+export {
+  DropdownMenu,
+  DropdownMenuCheckboxItem,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuPortal,
+  DropdownMenuRadioGroup,
+  DropdownMenuRadioItem,
+  DropdownMenuSeparator,
+  DropdownMenuShortcut,
+  DropdownMenuSub,
+  DropdownMenuSubContent,
+  DropdownMenuSubTrigger,
+  DropdownMenuTrigger,
+} from "./dropdown-menu"
+export { Input } from "./input"
+export { Label } from "./label"
+export {
+  Pagination,
+  PaginationContent,
+  PaginationEllipsis,
+  PaginationItem,
+  PaginationLink,
+  PaginationNext,
+  PaginationPrevious,
+} from "./pagination"
+export { Popover, PopoverContent, PopoverTrigger } from "./popover"
+export { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "./select"
+export { Separator } from "./separator"
+export { Sheet, SheetContent, SheetDescription, SheetFooter, SheetHeader, SheetTitle, SheetTrigger } from "./sheet"
+export { Skeleton } from "./skeleton"
+export { Tabs, TabsContent, TabsList, TabsTrigger } from "./tabs"
+export { Textarea } from "./textarea"
+export { Toast, ToastAction, ToastClose, ToastDescription, ToastProvider, ToastTitle, ToastViewport } from "./toast"
+export { Toaster } from "./toaster"
+export { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "./tooltip"
