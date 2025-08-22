e822d71d4b3eb7e74651fc3f2bdebd9879ae56ee
Yespecom <yespecomstudio@gmail.com>
2025-08-22 12:10:47 +0530

    Update global.d.ts

--- types/global.d.ts
+++ types/global.d.ts
@@ -1,10 +1,13 @@
 declare global {
-    interface Window {
-      grecaptcha: {
-        execute: (siteKey: string, options: { action: string }) => Promise<string>
-      }
+  interface Window {
+    grecaptcha: {
+      ready: (callback: () => void) => void
+      execute: (siteKey: string, options: { action: string }) => Promise<string>
+      render: (container: string | HTMLElement, parameters?: any) => number
+      reset: (widgetId?: number) => void
+      getResponse: (widgetId?: number) => string
     }
   }
-  
-  export {}
-  
+}
+
+export {}
